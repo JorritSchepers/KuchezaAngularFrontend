9f030f41d7a207a2bc9849a7eabaa922924ca4b1
Jorrit Schepers <joriit.schepers1@gmail.com>
2019-11-22 14:21:57 +0100

    Dingen weer terug gebracht

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component';
+import { RegisterApi } from './api/register.api';
+import { LoginApi } from './api/login.api';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+  ],
+  providers: [
+    RegisterApi,
+    LoginApi
+  ],
+  bootstrap: [
+    AppComponent
+  ]
+})
+export class AppModule {}
