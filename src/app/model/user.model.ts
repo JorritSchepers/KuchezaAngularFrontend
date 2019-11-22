b3a484c340134cfffb04aefa972ec6db45e78561
IndeWater <IndeWater@users.noreply.github.com>
2019-11-22 14:03:13 +0100

    userModel aangepast

--- src/app/model/user.model.ts
+++ src/app/model/user.model.ts
@@ -1,7 +1,12 @@
 import * as shajs from 'sha.js'
 
 export class UserModel {
-    constructor(private id: Number, private name: String, private password: String, private email: String) {
+  id: number;
+  name: String;
+  password: String;
+  email: String;
+
+  constructor(id: number, name: String, password: String, email: String) {
       this.id = id;
       this.name = name;
       this.password = shajs('sha256').update({password}).digest('hex');
