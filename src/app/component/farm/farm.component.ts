21ac2f80e217c3d37ca9647ada3bf18753b6909c
Jorrit Schepers <joriit.schepers1@gmail.com>
2019-11-26 10:44:51 +0100

    new PlotModel is nu duidelijker d.m.v. comments

--- src/app/component/farm/farm.component.ts
+++ src/app/component/farm/farm.component.ts
@@ -3,15 +3,15 @@ import { FarmApi } from 'src/app/api/farm.api';
 import { PlotModel } from 'src/app/model/plot.model';
 
 @Component({
-    selector: 'app-login',
     templateUrl: './farm.component.html',
     styleUrls: ['./farm.component.css']
   })
   export class FarmComponent {
-    grassPlot: PlotModel        = new PlotModel(1, 1, 1, 10, 0, 0, 0);
-    animalPlot: PlotModel       = new PlotModel(1, 1, 1, 10, 1, 0, 0);
-    waterManagerPlot: PlotModel = new PlotModel(1, 1, 1, 10, 0, 1, 0);
-    cropsPlot: PlotModel        = new PlotModel(2, 1, 2, 20, 0, 0, 1);
+                                              /*ID, x, y, price, animalID, wmID, plantID*/
+    grassPlot: PlotModel        = new PlotModel(1,  1, 1, 10,    0,        0,    0);
+    animalPlot: PlotModel       = new PlotModel(1,  1, 1, 10,    1,        0,    0);
+    waterManagerPlot: PlotModel = new PlotModel(1,  1, 1, 10,    0,        1,    0);
+    cropsPlot: PlotModel        = new PlotModel(2,  1, 2, 20,    0,        0,    1);
     plots: PlotModel[] = [this.grassPlot, this.animalPlot, this.waterManagerPlot, this.cropsPlot];
 
     constructor(private farmApi: FarmApi) { }
