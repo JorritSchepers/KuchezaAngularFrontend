eb1a172682826c52e73c83af0a37b67a8a6ce1f9
Jorrit Schepers <joriit.schepers1@gmail.com>
2019-11-29 11:32:57 +0100

    Login naming

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -3,19 +3,20 @@ import { FormBuilder } from '@angular/forms';
 import { LoginModel } from '../../model/login.model';
 import { TokenModel } from '../../model/token.model';
 import { LoginApi } from 'src/app/api/login.api';
+import { LoginResponseModel } from 'src/app/model/login-response.model';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
   email: String;
   password: String;
-  loginForm;
+  loginForm: any;
   token: String;
 
-  constructor(private formBuilder: FormBuilder, private loginApi: LoginApi) {
+  constructor(private formBuilder: FormBuilder, private loginApi: LoginApi, private router: Router) {
     this.loginForm = this.formBuilder.group({
       email: '',
       password: ''
@@ -23,9 +24,19 @@ export class LoginComponent {
     this.token = "Click here to see your token!";
   }
 
-  login(loginModel: LoginModel): void {
-    this.loginApi.query(new LoginModel(loginModel.email, loginModel.password));
+  loginUser(loginModel: LoginModel): void {
+    this.loginApi.login(new LoginModel(loginModel.email, loginModel.password)).then(response => this.handleLoginResponse(response))
+          .catch(any => this.handleLoginException(any));
     this.loginForm.reset();
+  }
+
+  private handleLoginResponse(response: LoginResponseModel): void {
+    TokenModel.setCurrentToken(response.token);
+    this.router.navigateByUrl('/farm');
+  }
+
+  private handleLoginException(exception: any): void {
+    console.warn("Exception:", exception);
   }
 
   setToken(): void {
