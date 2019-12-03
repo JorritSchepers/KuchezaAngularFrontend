2b204fb81a978a8d7c9580e5d902836c3a060971
SanneJelink <49753842+SanneJelink@users.noreply.github.com>
2019-12-03 18:55:25 +0100

    Replaced some placeholder assets

--- src/app/model/inventory.model.ts
+++ src/app/model/inventory.model.ts
@@ -3,7 +3,7 @@ export class InventoryModel {
     money: number;
     water: number;
 
-    constructor(money: float, water: float, userID: number) {
+    constructor(money: number, water: number, userID: number) {
         this.userID = userID;
         this.water = water;
         this.money = money;
