748cd9bc8bec37cae103f85cfdfc8d73fd3fc264
Jorrit Schepers <joriit.schepers1@gmail.com>
2019-12-06 13:42:42 +0100

    delet user klaar

--- src/app/component/admin/admin.component.ts
+++ src/app/component/admin/admin.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { AdminApi } from 'src/app/api/admin.api';
 import { UserModel } from 'src/app/model/user.model';
 import { AllUsersModel } from 'src/app/model/all-users.model';
-import { AllPlotModel } from 'src/app/model/allplot.model';
 
 @Component({
   templateUrl: './admin.component.html',
@@ -11,6 +10,7 @@ import { AllPlotModel } from 'src/app/model/allplot.model';
 export class AdminComponent {
   users: UserModel[] = Array<UserModel>();
   popUpIsActive: boolean = false;
+  currentSelectedUser: UserModel = null;
 
   constructor(private adminApi: AdminApi) {
     this.getAllNonAdminUsers();
@@ -25,13 +25,20 @@ export class AdminComponent {
     this.initUsers(response);
   }
 
-  showDeletePopUp(userID: number) {
+  showDeletePopUp(user: UserModel) {
+    this.currentSelectedUser = user;
     this.popUpIsActive = true;
+  }
+
+  closeDeletePopUp() {
+    this.currentSelectedUser = null;
+    this.popUpIsActive = false;
   }
   
   deleteUser(userID: number) {
     this.adminApi.deleteUser(userID).then(response => this.handleDeleteUserResponse(response))
     .catch(exception => this.handleException(exception));
+    this.closeDeletePopUp();
   }
 
   private handleDeleteUserResponse(response: AllUsersModel) {
